c2f5cfa9e6749888166e5c4507242f622ca56c07
NPreis1021 <nicholaspreis@gmail.com>
2022-09-07 18:08:18 -0400

    Added item scripts to project.

---
+++ Metroidvania Item Collection/Assets/Scripts/ItemScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    public enum Item
+    {
+        HEALTH_UPGRADE,
+        DOUBLE_JUMP,
+        WALL_JUMP,
+        AIR_DASH
+    };
+
+    public MasterItemCheckScript itemCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemCheck.gameObject.GetComponent<MasterItemCheckScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Metroidvania Item Collection/Assets/Scripts/MasterItemCheckScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterItemCheckScript : MonoBehaviour
+{
+    public bool canDoubleJump = false;
+    public bool canDash = false;
+    public bool canWallJump = false;
+}
